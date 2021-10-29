ef26096d54d7736312d22fe8d9bc63ef5eec5fd2
mtlankenau <mtlankenau@gmail.com>
2021-10-29 17:47:38 -0400

    installed inquirer and wrote initial prompt questions in 'index.js' and some markdown syntax within template literal of generateMarkdown function

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
 const questions = [];
 
@@ -7,7 +8,54 @@ const questions = [];
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  return inquirer
+    .prompt([
+    {
+      type: 'input',
+      name: 'project',
+      message: 'What is the title of your project?'
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a description of the project.'
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Provide any pertinent installation instructions.'
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Provide information regarding usage of this project.'
+    },
+    {
+      type: 'input',
+      name: 'contribution',
+      message: 'Provide guidelines regarding how to contribute to this project.'
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'Provide instructions for testing this project.'
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Please enter your GitHub username.'
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Please enter your email address.'
+    }])
+    .then((answers) => {
+      console.log(answers)
+      generateMarkdown(answers);
+    })
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,11 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (!license) {
+    return '';
+  }
+  
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -13,7 +18,29 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+## Description
+${data.description}
 
+## Table of Contents
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+
+## Contributing
+${data.contribution}
+
+## Tests
+${data.tests}
+
+## Questions
+Have more questions about this particular project?
+Feel free to check out my GitHub repository for this particular project at github.com/${data.github}/${data.project}.
+Also please visit my GitHub profile at github.com/${data.github}.
 `;
 }
 
